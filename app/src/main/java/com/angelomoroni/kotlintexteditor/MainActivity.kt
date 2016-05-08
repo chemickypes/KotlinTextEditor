b572bb03fbd104c4e73b2809062eaa8f3f5c7e6f
Angelo Moroni <mor.angelo.mor@gmail.com>
2016-05-08 22:09:30 +0200

    note activity

--- app/src/main/java/com/angelomoroni/kotlintexteditor/MainActivity.kt
+++ app/src/main/java/com/angelomoroni/kotlintexteditor/MainActivity.kt
@@ -39,25 +39,7 @@ class MainActivity : AppCompatActivity() {
                 updateNote(n)})
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true
-        }
-
-        return super.onOptionsItemSelected(item)
-    }
 
     fun getFakeNoteList() : List<Note>{
         var list = ArrayList<Note>();
