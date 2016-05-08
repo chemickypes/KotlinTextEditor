81ddbb7ad6c5049498b7d332e33f18ff1a7cf09e
Angelo Moroni <mor.angelo.mor@gmail.com>
2016-05-08 19:58:01 +0200

    create activity

---
+++ app/src/main/java/com/angelomoroni/kotlintexteditor/NoteActivity.kt
@@ -0,0 +1,12 @@
+package com.angelomoroni.kotlintexteditor
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NoteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note)
+    }
+}
