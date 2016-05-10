bcb41c42cb286ebe01d84087ada89ed13eabcb72
Angelo Moroni <mor.angelo.mor@gmail.com>
2016-05-10 22:41:11 +0200

    permessi

--- app/src/main/java/com/angelomoroni/kotlintexteditor/MainActivity.kt
+++ app/src/main/java/com/angelomoroni/kotlintexteditor/MainActivity.kt
@@ -6,15 +6,12 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.Toolbar
 import android.text.Editable
 import android.view.LayoutInflater
-import android.view.Menu
-import android.view.MenuItem
 import android.widget.EditText
 import android.widget.Toast
 import com.angelomoroni.kotlintexteditor.adapters.NoteAdapter
